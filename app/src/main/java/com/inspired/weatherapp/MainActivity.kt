f4205085f603d9c88449caea762d9a399ba3949c
Muhammed Bilal <bilalmuhammed363@gmail.com>
2024-02-17 21:53:30 +0530

    button ,show toast added

--- app/src/main/java/com/inspired/weatherapp/MainActivity.kt
+++ app/src/main/java/com/inspired/weatherapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.inspired.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnShowToast = findViewById<Button>(R.id.btnShowToast)
+
+        btnShowToast.setOnClickListener(){
+            Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT,).show();
+        }
+
     }
 }
