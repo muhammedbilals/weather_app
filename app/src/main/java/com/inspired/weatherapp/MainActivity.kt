5d84eba6a3cbd4d51998e35de4013eb66dc19a7e
Muhammed Bilal <bilalmuhammed363@gmail.com>
2024-02-17 22:23:15 +0530

    navigation done

--- app/src/main/java/com/inspired/weatherapp/MainActivity.kt
+++ app/src/main/java/com/inspired/weatherapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.inspired.weatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 
@@ -12,9 +14,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnShowToast = findViewById<Button>(R.id.btnShowToast)
+        val btnToSentText = findViewById<Button>(R.id.btnToSentText)
+        val etUserMessage = findViewById<EditText>(R.id.etUserMessage)
+
 
         btnShowToast.setOnClickListener(){
             Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT,).show();
+        }
+        btnToSentText.setOnClickListener(){
+            Toast.makeText(this,"Second button clicked",Toast.LENGTH_SHORT,).show();
+            val message :String = etUserMessage.text.toString();
+            Toast.makeText(this,message,Toast.LENGTH_SHORT,).show();
+            val intent =Intent(this,SecondActivity::class.java)
+            startActivity(intent)
         }
 
     }
